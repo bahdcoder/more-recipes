940e219b1dec8e0f3d9e74fdf743fa7b1297bb46
kati-frantz <frantzenterprises777@gmail.com>
2017-11-02 10:10:54 +0100

    update views directory

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => res.render('index'));

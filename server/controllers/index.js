5376a699a1fddd1e3a1c69bddf968404ca8ca4a9
kati-frantz <frantzenterprises777@gmail.com>
2017-11-02 06:59:19 +0100

    update controllers.index to export users.controller and auth.controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,13 @@
 import ReviewsController from './reviews.controller';
 import RecipesController from './recipes.controller';
 import VotesController from './votes.controller';
+import AuthController from './auth.controller';
+import UsersController from './users.controller';
 
 export default {
   RecipesController,
   ReviewsController,
-  VotesController
+  VotesController,
+  UsersController,
+  AuthController
 };

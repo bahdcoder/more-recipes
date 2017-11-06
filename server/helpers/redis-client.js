6e802063e0eb4af33de9ee81fc89bc47422e6bb1
kati-frantz <frantzenterprises777@gmail.com>
2017-11-06 12:09:09 +0100

    add custom fake redis database to project

--- server/helpers/redis-client.js
+++ server/helpers/redis-client.js
@@ -1,5 +1,5 @@
-import fakeredis from 'fakeredis';
 import { createClient } from 'then-redis';
+import FakeRedis from './redis/fakeRedis';
 /**
  * Create a new client redis
  * @returns {object} fake-redis client for testing and real redis-client in production
@@ -9,7 +9,7 @@ const getRedisClient = () => {
     return createClient('redis://h:pc47f9fe9063e2c77c2c0b4620873e24b207d1bea3c07e6b25c65ec1f878feb1e@ec2-34-199-160-190.compute-1.amazonaws.com:31909');
   }
 
-  return createClient();
+  return new FakeRedis();
 };
 
 

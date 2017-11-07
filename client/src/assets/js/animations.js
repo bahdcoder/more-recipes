9e2cf5c9a2497b2482ff5cc93ab6a0785a45ded2
kati-frantz <frantzenterprises777@gmail.com>
2017-11-07 14:58:05 +0100

    add home page from template to react component

---
+++ client/src/assets/js/animations.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+$("#jumbotron").vegas({
+    timer: false,
+    shuffle: true,
+    transition: 'fade',
+    animation: 'kenburns',
+    slides: [
+        { src: './../img/banner-1.jpg' },
+        { src: './../img/banner-2.jpg' },
+        { src: './../img/banner-3.jpg' }
+    ]
+});
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../css/bootstrap.min.css';
+import '../css/animate.min.css';
 import '../css/styles.css';
 
 import Home from './pages/Home';
-import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('app'))
-registerServiceWorker()
+ReactDOM.render(<Home />, document.getElementById('app'));
+registerServiceWorker();

5b62d8e44b2e06a1cdfaa863579e2d7afb7de00c
kati-frantz <frantzenterprises777@gmail.com>
2017-11-19 13:53:13 +0100

    add delete public directory script before building and deploying

---
+++ server/scripts/removeDeploymentDir.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+//  const nodePath = require('path');
+
+const deleteFolderRecursive = (path) => {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach((file) => {
+      const curPath = `${path}/${file}`;
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
+deleteFolderRecursive(`${__dirname}/../public`);

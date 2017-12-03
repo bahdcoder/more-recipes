2cee20625531681a926cef032f029793249d2586
kati-frantz <frantzenterprises777@gmail.com>
2017-12-03 09:03:44 +0100

    fix build

--- server/test/feature/recipes.spec.js
+++ server/test/feature/recipes.spec.js
@@ -46,18 +46,18 @@ describe('/recipes', () => {
 
   describe('/recipes GET endpoint', () => {
 
-    it('Should return a list of recipes when called', (done) => {
+    it.only('Should return a list of recipes when called', (done) => {
       chai.request(application)
         .get('/api/v1/recipes')
         .end((error, response) => {
           expect(response).to.have.status(200);
           const res = response.body;
+          const { recipes } = res.data.recipes;
 
-          expect(res.data.recipes).to.not.be.undefined;
-          expect(Array.isArray(res.data.recipes)).to.be.true;
-          expect(res.data.recipes[0].id).to.equal(globalMock.recipe1.id);
-          expect(res.data.recipes[0].title).to.equal(globalMock.recipe1.title);
-          expect(res.data.recipes[0].User.name).to.equal(globalMock.user1.name);      
+          expect(Array.isArray(recipes)).to.be.true;
+          expect(recipes[0].id).to.equal(globalMock.recipe1.id);
+          expect(recipes[0].title).to.equal(globalMock.recipe1.title);
+          expect(recipes[0].User.name).to.equal(globalMock.user1.name);      
           done();
         });
     });

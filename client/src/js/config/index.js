1f09e03bec5727e059d27a56b409f138689fbff0
Kati Frantz <frantzenterprises777@gmail.com>
2018-11-03 16:19:08 +0100

    Update index.js

--- client/src/js/config/index.js
+++ client/src/js/config/index.js
@@ -3,7 +3,7 @@ let apiUrl;
 if (window.location.hostname === 'bahdcoder-more-recipes.herokuapp.com') {
   apiUrl = 'https://bahdcoder-more-recipes.herokuapp.com/api/v1';
 } else {
-  apiUrl = 'http://localhost:4080/api/v1';
+  apiUrl = '/';
 }
 /**
  * Export application wide configurations
